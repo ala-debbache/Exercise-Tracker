52fa2f2249abf937295594cc497e4dc5e03daa12
ala-debbache <65853803+ala-debbache@users.noreply.github.com>
2021-09-11 19:14:26 +0100

    1.0

--- server.js
+++ server.js
@@ -13,11 +13,21 @@ app.use(express.static('public'))
 mongoose.connect("mongodb+srv://ala:maradona2018@exercises.axzra.mongodb.net/exercises?retryWrites=true&w=majority");
 
 const { Schema } = mongoose;
+
+
 const userSchema = new Schema({
   username: String
 });
 
+const exerciseSchema = new Schema({
+  username: String,
+  description: String,
+  duration: Number,
+  date: String
+});
+
 const User = mongoose.model("User",userSchema);
+const Exercise = mongoose.model("Exercise",exerciseSchema);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
@@ -28,7 +38,68 @@ app.post("/api/users",async (req,res)=>{
   try {
     let user = User({username: username});
     await user.save();
-    res.json(user);
+    res.json({username: user.username, _id: user.id});
+  } catch {
+    res.status(401).json({error: "server or database error"});
+  }
+});
+
+app.get("/api/users",async (req,res)=>{
+  try {
+    const all = await User.find({});
+    res.json(all);
+  } catch {
+    res.status(401).json({error: "server or database error"});
+  }
+});
+
+app.post("/api/users/:_id/exercises",async (req,res)=>{
+  let description = req.body.description;
+  let duration = req.body.duration;
+  let date = "";
+  if(req.body.date){
+    date = new Date(req.body.date).toDateString();
+  }else{
+    date = new Date().toDateString();
+  }
+  try {
+    const user = await User.findOne({_id: req.params._id});
+    let exercise = new Exercise({
+      username: user.username,
+      description: description,
+      duration: duration,
+      date: date
+    });
+    await exercise.save();
+    res.json({
+      _id: user._id,
+      username: user.username,
+      description: description,
+      duration: duration,
+      date: date
+    });
+  } catch {
+    res.status(401).json({error: "server or database error"});
+  }
+});
+
+app.get("/api/users/:id/logs",async (req,res)=>{
+  try {
+    let user = await User.findOne({_id: req.params.id});
+    let exercises = await Exercise.find({username: user.username});
+    let logs = exercises.map(e=>{
+      return {
+        description: e.description,
+        duration: e.duration,
+        date: e.date
+      }
+    });
+    res.json({
+      username: user.username,
+      _id: req.params.id,
+      count: exercises.length,
+      log: [logs]
+    });
   } catch {
     res.status(401).json({error: "server or database error"});
   }
